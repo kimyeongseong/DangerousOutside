8d62bd35b38b0f0114545dcfd6246388b82b346c
kimyeongseong <63996607+kimyeongseong@users.noreply.github.com>
2021-04-13 22:32:28 +0900

    매니저 생성 및 메뉴바 스테이지 새로 작업중..
    으엉 소스가 마음에 안들어요

---
+++ Assets/StageSelectMng.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectMng : MonoBehaviour
+{
+    public Sprite[] stageSelectImg=null;
+    int arrow_dir=0;
+    public Image bgImg = null;
+    public GameObject bgImgPrb=null;
+    public List<GameObject> bgPrbList=null;
+    public GameObject canvas=null;
+    public void Awake()
+    {
+        stageSelectImg=Resources.LoadAll<Sprite>("StageMapImg");
+        foreach(var num in stageSelectImg)
+        {
+           GameObject bgPrb= Instantiate(bgImgPrb, Vector3.zero,Quaternion.identity, canvas.transform);
+           bgPrbList.Add(bgPrb);
+        }
+    }
+    public void Arrow()
+    {
+        
+    }
+}
